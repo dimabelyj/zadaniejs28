b98945278ff31bce1147c37bd46a04024eb39461
Dmytro Bilokolodskykh <belokolodskikh@gmail.com>
2019-03-26 22:27:20 +0100

    finisz

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let x = 10
+if (x > 10) {
+    console.log('Podana liczba jest większa od 10');
+} else if (x < 10) {
+    console.log('Podana liczba nie jest większa od 10');
+} else if (x = 10) {
+    console.log('Podana liczba równia sie 10 ');
+}
